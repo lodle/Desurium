644a65babde078f285d35869ef392396b8ec82f0
Lodle <mark@moddb.com>
2013-10-10 15:22:13 -0700

    Fixed breaking unit tests and bugs in McfManager

--- src/shared/usercore/code/McfManager.cpp
+++ src/shared/usercore/code/McfManager.cpp
@@ -137,7 +137,7 @@ void MCFManager::getListOfBadMcfPaths(sqlite3x::sqlite3_connection &db, std::vec
 		{
 			gcString path = reader.getstring(2);
 
-			if (path.find(m_szAppDataPath) != 0)
+			if (path.find(m_szAppDataPath) != std::string::npos)
 			{
 				MigrateInfo mi;
 
@@ -598,6 +598,7 @@ void MCFManager::validateMcf(const char* mcf)
 
 	DesuraId id = mcfHead->getDesuraId();
 	MCFBuild build = mcfHead->getBuild();
+	MCFBranch branch = mcfHead->getBranch();
 
 	bool isPatch = strstr(mcf, "patch")?true:false;
 	bool isUnAuthed = strstr(mcf, "unauth")?true:false;
@@ -612,7 +613,8 @@ void MCFManager::validateMcf(const char* mcf)
 		cmd.bind(1, (long long int)id.toInt64());
 		cmd.bind(2, (int)build);
 		cmd.bind(3, UTIL::OS::getRelativePath(mcf));
-		cmd.bind(4, (int)flags);
+		cmd.bind(4, (int)branch);
+		cmd.bind(5, (int)flags);
 
 		cmd.executenonquery();
 	}
@@ -681,7 +683,8 @@ namespace UnitTest
 
 	TEST_F(MCFManagerFixture, getListOfBadMcfPaths)
 	{
-		sqlite3x::sqlite3_connection db;
+		sqlite3x::sqlite3_connection db(":memory:");
+		db.executenonquery(CREATE_MCFITEM);
 
 		auto insertItem = [&db](DesuraId id, gcString strPath, bool bUnAuthed) -> void 
 		{
@@ -691,19 +694,21 @@ namespace UnitTest
 				cmd.bind(1, (long long int)id.toInt64());
 				cmd.bind(2, 0);
 				cmd.bind(3, strPath);
-				cmd.bind(4, bUnAuthed ? FLAG_UNAUTHED:0);
+				cmd.bind(4, 0);
+				cmd.bind(5, bUnAuthed ? FLAG_UNAUTHED : 0);
 
 				cmd.executenonquery();
 			}
-			catch (std::exception &)
+			catch (std::exception &e)
 			{
+				int a=1;
 			}
 		};
 
-		insertItem(DesuraId("1", "games"), "mcfroot//test1.mcf", true);
-		insertItem(DesuraId("2", "games"), "abcd//test2.mcf", false);
-		insertItem(DesuraId("3", "games"), "appdata//test3.mcf", false);
-		insertItem(DesuraId("4", "games"), "mcfroot//test4.mcf", false);
+		insertItem(DesuraId("51", "games"), "appdata\\test1.mcf", true);
+		insertItem(DesuraId("62", "games"), "appdata\\test2.mcf", false);
+		insertItem(DesuraId("73", "games"), "appdata\\test3.mcf", false);
+		insertItem(DesuraId("84", "games"), "mcfroot\\test4.mcf", false);
 
 		std::vector<UserCore::MigrateInfo> delList;
 		std::vector<UserCore::MigrateInfo> updateList;
@@ -711,11 +716,11 @@ namespace UnitTest
 		getListOfBadMcfPaths(db, delList, updateList);
 
 		ASSERT_EQ(1, delList.size());
-		ASSERT_EQ(DesuraId("1", "games"), delList[0].id);
+		ASSERT_EQ(DesuraId("51", "games"), delList[0].id);
 
 		ASSERT_EQ(2, updateList.size());
-		ASSERT_EQ(DesuraId("2", "games"), updateList[0].id);
-		ASSERT_EQ(DesuraId("3", "games"), updateList[1].id);
+		ASSERT_EQ(DesuraId("62", "games"), updateList[0].id);
+		ASSERT_EQ(DesuraId("73", "games"), updateList[1].id);
 	}
 }
 
