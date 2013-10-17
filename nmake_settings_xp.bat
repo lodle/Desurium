d7f4acd38ad82ed0d0f5baa959a2604d3ebac15c
Lodle <mark@moddb.com>
2013-10-17 16:05:16 -0700

    Added v110 settings for xp builds using nmake

---
+++ nmake_settings_xp.bat
@@ -0,0 +1,4 @@
+set INCLUDE=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Include;%INCLUDE%
+set PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Bin;%PATH%
+set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;%LIB%
+set CL=/D_USING_V110_SDK71_;%CL%
